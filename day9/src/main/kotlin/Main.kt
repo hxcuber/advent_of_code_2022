71d23c27e18856fa1152dce8e4bf858742d9b828
Hoang Nguyen <hn622@ic.ac.uk>
2022-12-09 10:45:23 +0000

    day 9 started

---
+++ day9/src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+import java.io.File
+
+fun main(args: Array<String>) {
+    val test = File("input.txt").bufferedReader()
+
+    test.forEachLine {
+        if (it == "") {
+
+        } else {
+
+        }
+    }
+
+}
