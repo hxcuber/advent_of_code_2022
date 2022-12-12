55b49f9adfa11f338176fb12b7af9cfb3be8d688
Hoang Nguyen <hn622@ic.ac.uk>
2022-12-12 00:55:27 +0000

    day 11 part 2 done, had to mod with the LCM of all the checks

--- day11/src/main/kotlin/Main.kt
+++ day11/src/main/kotlin/Main.kt
@@ -12,28 +12,30 @@ fun main(args: Array<String>) {
         file.readLine()
         game.addMonkey(Monkey(items.toMutableList(), operation, test))
     }
-    game.play(20)
+    game.play(10000)
 
-    // part 1
-    println(game.monkeyList.sortedByDescending { it.inspections }[0].inspections *
-            game.monkeyList.sortedByDescending { it.inspections }[1].inspections)
+    // part 1 - play game 20 times, div by 3 for worry level
+    // part 2 - play game 10000 times, no div by 3 (probably using Longs instead of Ints for this)
+    println(game.monkeyList.sortedByDescending { it.inspections }[0].inspections * game.monkeyList.sortedByDescending { it.inspections }[1].inspections
+    )
+
 }
 
 class Monkey(
-    var items: MutableList<Int> = mutableListOf(),
-    val operation: (Int) -> Int,
-    val test: (Int) -> Int,
-    var inspections: Int = 0
+    var items: MutableList<Long> = mutableListOf(),
+    val operation: (Long) -> Long,
+    val test: (Long) -> Long,
+    var inspections: Long = 0
 ) {
     // (item, monkey)
-    fun inspect(): List<Pair<Int, Int>> {
+    fun inspect(): List<Pair<Long, Long>> {
         val itemsCopy = items
         items = mutableListOf()
         inspections += itemsCopy.size
-        return itemsCopy.map { operation(it) / 3 to test(operation(it) / 3) }
+        return itemsCopy.map { operation(it) % (5*17*2*7*3*11*13*19) to test(operation(it) % (5*17*2*7*3*11*13*19)) }
     }
 
-    fun addItem(item: Int) {
+    fun addItem(item: Long) {
         items.add(item)
     }
 }
@@ -41,42 +43,34 @@ class Monkey(
 class MonkeyGame(val monkeyList: MutableList<Monkey> = mutableListOf()) {
     fun addMonkey(monkey: Monkey) = monkeyList.add(monkey)
 
-    fun play(rounds: Int = 1) {
-        when (rounds) {
-            0 -> {}
-            else -> {
-                for (monkey in monkeyList) {
-                    val listToAdd = monkey.inspect()
-                    listToAdd.map { (i, m) -> monkeyList[m].addItem(i) }
-                }
-                for (monkey in monkeyList) {
-                    println(monkey.items)
-                }
-                println("after round $rounds")
-                play(rounds - 1)
+    fun play(rounds: Long = 1) {
+        for (i in 1..rounds) {
+            for (monkey in monkeyList) {
+                val listToAdd = monkey.inspect()
+                listToAdd.map { (i, m) -> monkeyList[m.toInt()].addItem(i) }
             }
         }
     }
 }
 
 class MonkeyParser() {
-    fun parseItems(line: String): List<Int> {
-        return line.substringAfter("Starting items:").split(",").map { it.trim().toInt() }
+    fun parseItems(line: String): List<Long> {
+        return line.substringAfter("Starting items: ").split(",").map { it.trim().toLong() }
     }
 
-    fun parseOperation(line: String): (Int) -> Int {
+    fun parseOperation(line: String): (Long) -> Long {
         val opNum = line.substringAfter("Operation: new = old ").split(" ")
         return when (opNum[0]) {
-            "*" -> { x -> x * if (opNum[1] == "old") x else opNum[1].toInt() }
-            "+" -> { x -> x + if (opNum[1] == "old") x else opNum[1].toInt() }
+            "*" -> { x -> x * if (opNum[1] == "old") x else opNum[1].toLong() }
+            "+" -> { x -> x + if (opNum[1] == "old") x else opNum[1].toLong() }
             else -> { x -> x }
         }
     }
 
-    fun parseTest(line1: String, line2: String, line3: String): (Int) -> Int {
-        val test = line1.substringAfter("Test: divisible by ").toInt()
-        val ifTrue = line2.substringAfter("If true: throw to monkey ").toInt()
-        val ifFalse = line3.substringAfter("If false: throw to monkey ").toInt()
-        return { if (it % test == 0) ifTrue else ifFalse }
+    fun parseTest(line1: String, line2: String, line3: String): (Long) -> Long {
+        val test = line1.substringAfter("Test: divisible by ").toLong()
+        val ifTrue = line2.substringAfter("If true: throw to monkey ").toLong()
+        val ifFalse = line3.substringAfter("If false: throw to monkey ").toLong()
+        return { if (it % test == 0L) ifTrue else ifFalse }
     }
 }
